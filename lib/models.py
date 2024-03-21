4b5f272c10c13a3ea9e541efb6c6f1ea85172231
CubicPower <55009831+markhsia@users.noreply.github.com>
2024-03-21 15:12:57 +0800

    Update models.py

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,8 @@
 from . import graph
 
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import sklearn
 import scipy.sparse
 import numpy as np
